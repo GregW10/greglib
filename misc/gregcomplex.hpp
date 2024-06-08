7ddeb10dedcde0d1317249b3d2b106a7c2e001dd
Gregor Hartl Watters <mario.garwh@gmail.com>
2024-06-08 19:21:54 +0100

    Some minor additions, such as electric field to intensity (and vice versa) functions.

--- misc/gregcomplex.hpp
+++ misc/gregcomplex.hpp
@@ -21,6 +21,7 @@ namespace gtd {
 	    T _imag{};
     public:
 	    HOST_DEVICE complex() = default;
+        HOST_DEVICE complex(const T &_real_) : _real{_real_} {}
 	    HOST_DEVICE complex(const T &_real_, const T &_imag_) : _real{_real_}, _imag{_imag_} {}
         HOST_DEVICE complex(const complex<T> &other) : _real{other._real}, _imag{other._imag} {}
         HOST_DEVICE complex(complex<T> &&other) : _real{std::move(other._real)}, _imag{std::move(other._imag)} {} // moving is almost certainly overkill
@@ -58,6 +59,15 @@ namespace gtd {
                 throw invalid_complex_operation{"Error: the argument of the zero complex number is undefined.\n"};
             return std::acos(this->_real/this->mag()); */
         }
+        HOST_DEVICE complex<T> conj() const noexcept {
+            return {this->_real, -this->_imag};
+        }
+        HOST_DEVICE complex<decltype(+std::declval<T>())> operator+() const noexcept {
+            return {+this->_real, +this->_imag};
+        }
+        HOST_DEVICE complex<T> operator-() const noexcept {
+            return {-this->_real, -this->_imag};
+        }
     	HOST_DEVICE complex<T> &operator+=(const complex<T> &other) {
 		    this->_real += other._real;
 	    	this->_imag += other._imag;
@@ -82,6 +92,28 @@ namespace gtd {
 	    	this->_real += scalar;
 	    	return *this;
 	    }
+        HOST_DEVICE complex<T> &operator=(const T &_real_) {
+            this->_real = _real_;
+            this->_imag = 0;
+            return *this;
+        }
+        HOST_DEVICE complex<T> &operator=(const complex<T> &other) {
+            if (&other == this)
+                return *this;
+            this->_real = other._real;
+            this->_imag = other._imag;
+            return *this;
+        }
+        HOST_DEVICE complex<T> &operator=(complex<T> &&other) {
+            if (&other == this)
+                return *this;
+            this->_real = std::move(other._real);
+            this->_imag = std::move(other._imag);
+            return *this;
+        }
+        HOST_DEVICE explicit operator T() {
+            return this->_real;
+        }
         HOST_DEVICE friend complex<T> operator*(const T &s, const complex<T> &c) {
             return {s*c._real, s*c._imag};
         }
@@ -106,20 +138,6 @@ namespace gtd {
         HOST_DEVICE friend complex<T> operator*(const complex<T> &c1, const complex<T> &c2) {
             return {c1._real*c2._real - c1._imag*c2._imag, c1._real*c2._imag + c1._imag*c2._real};
         }
-        HOST_DEVICE complex<T> &operator=(const complex<T> &other) {
-	        if (&other == this)
-	            return *this;
-	        this->_real = other._real;
-	        this->_imag = other._imag;
-	        return *this;
-	    }
-        HOST_DEVICE complex<T> &operator=(complex<T> &&other) {
-	        if (&other == this)
-	            return *this;
-	        this->_real = std::move(other._real);
-	        this->_imag = std::move(other._imag);
-	        return *this;
-	    }
         friend std::ostream &operator<<(std::ostream &os, const complex<T> &c) {
 	        os << +c._real;
 	        if (c._imag >= 0)
